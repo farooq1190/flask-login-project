5f9e03878ac5aa0d2526ba55bf2b472d20a01f0c
farooq1190 <fkfarooqkhan1190@gmail.com>
2026-01-08 19:46:00 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,45 @@
+from flask import Flask, render_template, request
+from flask import session, redirect, url_for
+
+app = Flask(__name__)
+
+app.secret_key = 'secret123'
+
+USER = {
+    'username': 'admin',
+    'password': '1234'
+}
+
+@app.route('/')
+def home():
+    return "Home Page"
+
+@app.route('/login', methods =['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+    
+        if username == USER['username'] and password == USER['password']:
+            session['user'] = username
+            return redirect(url_for('dashboard'))
+            #return "Login successful!"
+        else:
+            return "Invalid credentials, please try again."
+        # Add your login logic here
+        #return f"username: {username}, password: {password}"
+    return render_template('login.html')  # Ensure you have a login.html template in the templates folder
+
+@app.route('/dashboard')
+def dashboard():
+    if 'user' in session:
+        return render_template('dashboard.html')
+    return redirect(url_for('login'))
+
+@app.route('/logout')
+def logout():
+    session.pop('user', None)
+    return redirect(url_for('login'))
+
+if __name__ == "__main__":
+    app.run(debug=True)
